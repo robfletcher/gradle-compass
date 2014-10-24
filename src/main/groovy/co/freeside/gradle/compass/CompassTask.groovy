f01da8577d6c4db561a25640270aefda2a77c3ec
Rob Fletcher <rfletcher@netflix.com>
2014-10-23 23:40:37 -0700

    tidier implementation of CompassTask

--- src/main/groovy/co/freeside/gradle/compass/CompassTask.groovy
+++ src/main/groovy/co/freeside/gradle/compass/CompassTask.groovy
@@ -1,8 +1,11 @@
 package co.freeside.gradle.compass
 
 import com.github.jrubygradle.JRubyExec
+import com.github.jrubygradle.internal.JRubyExecUtils
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.OutputDirectory
+
+import static co.freeside.gradle.compass.CompassPlugin.CONFIGURATION_NAME
 
 class CompassTask extends JRubyExec {
 
@@ -11,15 +14,30 @@ class CompassTask extends JRubyExec {
   @OutputDirectory File cssDir
 
   CompassTask() {
-    jrubyArgs "-S"
     script = new File("compass")
-    configuration = "compass"
+    configuration = CONFIGURATION_NAME
   }
 
   @Override
-  void exec() {
-    scriptArgs command, "--sass-dir", getSassDir(), "--css-dir", getCssDir()
-    println "Using: ${scriptArgs()}"
-    super.exec()
+  List<String> jrubyArgs() {
+    def jrubyArgs = super.jrubyArgs()
+    if (!jrubyArgs.contains("-S")) {
+      jrubyArgs << "-S"
+    }
+    return jrubyArgs
+  }
+
+  @Override
+  List<String> scriptArgs() {
+    def scriptArgs = [command]
+    scriptArgs << "--sass-dir" << getSassDir().path
+    scriptArgs << "--css-dir" << getCssDir().path
+    scriptArgs.addAll(super.scriptArgs())
+    return scriptArgs
+  }
+
+  @Override
+  List<String> getArgs() {
+    JRubyExecUtils.buildArgs(jrubyArgs(), script, scriptArgs())
   }
 }
